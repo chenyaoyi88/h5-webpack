ea22dcd6de4aa9992b17c657f3cbcf995fda5b8c
chenyaoyi88 <152296058@qq.com>
2018-02-13 18:01:22 +0800

    优化上传配置

--- config/project.upload.js
+++ config/project.upload.js
@@ -16,11 +16,11 @@ const ftpConfig = require('../config.ftp.json');
 // 环境变量
 const ENV = process.env.NODE_ENV;
 
-const configInfo = require('../confgi.info.json');
+const configInfo = require('../config.info.json');
 // 远程基本路径
 const remoteBasePath = './pub/ghb-web/act';
 // 远程层级路径，如：/2018/08/08/appName
-const remotePathLevel = `/${configInfo.onlineYear}/${configInfo.onlineMonth}/${configInfo.onlineDate}/${configInfo.appName}`;
+const remotePathLevel = `/${configInfo.onlineYear}/${configInfo.onlineMonth}/${configInfo.appName}`;
 
 // 完整的上传目录，下面例子是上传到 ftp 的 ./pub/ghb-web/act/2018/08/08/appName 文件夹下
 const entireBasePath = remoteBasePath + remotePathLevel;
@@ -43,25 +43,25 @@ let aUploadSuccess = [];
 // 上传失败的文件
 let aUploadFailed = [];
 
-log(chalk.yellow('当前环境路径：') + chalk.green(curEnvDir));
+log('当前环境路径：' + chalk.green(curEnvDir));
 
-readDirLoop(curEnvDir, function(err, data) {
+readDirLoop(curEnvDir, function (err, data) {
   aFiles = data;
 });
 
 // 延迟 1 秒连接 ftp，因为上面 readDirLoop 递归计算文件个数需要时间，而且不知道什么时候计算完
-setTimeout(function() {
-  console.log(`约 ${chalk.green(aFiles.length)} 个文件需要上传`);
+setTimeout(function () {
 
-  ftp.on('ready', function() {
+  ftp.on('ready', function () {
     log(chalk.green('ftp 连接成功'));
     createProjectBaseDir(entireBasePath).then(() => {
+      console.log(`约 ${chalk.green(aFiles.length)} 个文件需要上传`);
       log(chalk.green('开始上传文件...'));
       readUploadDir(curEnvDir, entireBasePath);
     });
   });
 
-  ftp.on('error', function(err) {
+  ftp.on('error', function (err) {
     log(chalk.red('ftp 连接失败'));
   });
 
@@ -76,15 +76,15 @@ setTimeout(function() {
  */
 function createProjectBaseDir(basePath) {
   return new Promise((resolve, reject) => {
-    ftp.mkdir(basePath, true, function(err) {
+    ftp.mkdir(basePath, true, function (err) {
       if (err) {
-        console.log(chalk.red('在 FTP 上创建本项目文件夹失败：'), err);
+        console.log(chalk.red('在 FTP 上创建本项目文件夹失败：\n'), err);
         reject(err);
         ftp.end();
         return;
       }
       log(
-        chalk.yellow('在 FTP 上创建本项目文件夹成功：') + chalk.green(basePath)
+        '在 FTP 上创建本项目文件夹成功：' + chalk.green(basePath)
       );
       resolve();
     });
@@ -100,7 +100,7 @@ function createProjectBaseDir(basePath) {
  */
 function uploadFile(uploadPath, remotePath, file) {
   const remoteFilePath = remotePath + '/' + file;
-  ftp.put(path.join(uploadPath, file), remoteFilePath, function(err) {
+  ftp.put(path.join(uploadPath, file), remoteFilePath, function (err) {
     if (err) {
       console.log(file, chalk.red('上传文件失败：'), err);
       console.log(chalk.yellow('尝试重新上传：'), file);
@@ -112,6 +112,7 @@ function uploadFile(uploadPath, remotePath, file) {
     if (aUploadSuccess.length === aFiles.length) {
       ftp.end();
       log(chalk.green('上传完成'));
+      console.log('URL:', chalk.green(`https://sit.guanghuobao.com/ghb-web/act${remotePathLevel}/index.html`));
     }
   });
 }
@@ -123,7 +124,7 @@ function uploadFile(uploadPath, remotePath, file) {
  * @param {any} remotePath 上传的文件的远程路径
  */
 function readUploadDir(uploadPath, remotePath) {
-  fs.readdir(uploadPath, function(err, files) {
+  fs.readdir(uploadPath, function (err, files) {
     if (err) {
       console.log(chalk.red('读取文件夹错误：'), err);
       ftp.end();
@@ -131,7 +132,7 @@ function readUploadDir(uploadPath, remotePath) {
     }
     files.forEach((file, index) => {
       let readLocalFile = path.join(uploadPath, file);
-      fs.stat(readLocalFile, function(err, stat) {
+      fs.stat(readLocalFile, function (err, stat) {
         if (err) {
           console.log(chalk.red('检测文件夹错误：'), err);
           ftp.end();
@@ -143,7 +144,7 @@ function readUploadDir(uploadPath, remotePath) {
         } else if (stat.isDirectory()) {
           // 如果是文件夹，递归处理
           const nextLevelRemoteFilePath = remotePath + '/' + file;
-          ftp.mkdir(nextLevelRemoteFilePath, true, function(err) {
+          ftp.mkdir(nextLevelRemoteFilePath, true, function (err) {
             if (err) {
               console.log(chalk.red('创建项目目录失败：'), err);
               return;
@@ -168,19 +169,19 @@ function readUploadDir(uploadPath, remotePath) {
  */
 function readDirLoop(uploadPath, callback) {
   let results = [];
-  fs.readdir(uploadPath, function(err, files) {
+  fs.readdir(uploadPath, function (err, files) {
     if (err) return callback(err);
     let pending = files.length;
     if (!pending) return callback(null, results);
     files.forEach(file => {
       file = path.join(uploadPath, file);
       // let readLocalFile = path.resolve(uploadPath, file);
-      fs.stat(file, function(err, stat) {
+      fs.stat(file, function (err, stat) {
         if (stat.isFile()) {
           results.push(file);
           if (!--pending) callback(null, results);
         } else if (stat.isDirectory()) {
-          readDirLoop(file, function(err, res) {
+          readDirLoop(file, function (err, res) {
             results = results.concat(res);
             if (!--pending) callback(null, results);
           });
